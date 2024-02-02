f83e72f278149d457697b9bd076e69ea705cab4b
Konstantin Ageev <Ageevkonstantin00@gmail.com>
2024-02-02 22:13:18 +0500

    I've just added some code

--- Program.cs
+++ Program.cs
@@ -3,7 +3,25 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!");
+          
+        string a = Console.ReadLine();
+        
+        double firstfigure = Convert.ToDouble(a);
+
+        string b = Console.ReadLine();
+
+        double secondfigure = Convert.ToDouble(b);
+
+        string c = Console.ReadLine();
+
+        double thirdfigure = Convert.ToDouble(c);
+
+        double sum = firstfigure + secondfigure + thirdfigure;
+
+        Console.WriteLine(sum); 
+
+        Console.WriteLine(sum); 
+ 
+  
     }
 }
